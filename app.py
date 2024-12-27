c36bf19c8e4f65de78977990502101bf71ec5240
Rohit <rohit.shinde9509@gmail.com>
2024-12-27 16:51:04 +0530

    another change

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 //Initialize the array that will hold the primes
-var primeArray = [2323];
+var primeArray = [000000];
 /*Write a function that checks for primeness and
 pushes those values to the array*/
 function PrimeCheck(candidate){
